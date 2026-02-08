1650c6aaf507faf8068c247315b35a6d5531d86f
delivaryvip-jpg <delivaryvip@gmail.com>
2026-02-08 23:39:57 +0300

    Initialize SQLite database and create users table

--- database.js
+++ database.js
@@ -1,1 +1,14 @@
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./points.db');
+
+db.run(`
+CREATE TABLE IF NOT EXISTS users (
+  id TEXT PRIMARY KEY,
+  name TEXT,
+  points INTEGER DEFAULT 0,
+  messages INTEGER DEFAULT 0
+)
+`);
+
+module.exports = db;
